36affb5410a724d6d9745c0759d8362bd49c624a
Saras Windecker <sm.windecker@unimelb.edu.au>
2019-09-22 17:41:34 +1000

    tidy up analysis script with headers

--- analysis.R
+++ analysis.R
@@ -1,15 +1,20 @@
 ## Analysis file
+
 library(raster)
 library(magrittr)
 library(mgcv)
 
+## Load R scripts
+
 R.utils::sourceDirectory('R/')
 
-# Processed spatial data files are provided in spatial/processed
+# Process raw spatial data
+## Processed spatial data files are provided in spatial/processed
 ## library(gdaltools)
 ## process_spatial_data()
 
-# data prep
+# Prepare data
+
 lab_data <- process_lab_results(lab_data = "data-raw/VWC_Lab_Results.csv")
 
 samples <- prepare_master_data(site_data = "data-raw/VWC_SitesCores_Masterlist.csv",
@@ -17,41 +22,42 @@ samples <- prepare_master_data(site_data = "data-raw/VWC_SitesCores_Masterlist.c
                                wetland_types = "data-raw/PC_carbon.csv",
                                lab_data = lab_data)
 
-# write.csv(samples[,c('SITE_CODE', 'LAT', 'LON')], paste0(path, 'samples_latlon.csv'))
+# Oranic carbon density GAM
 
-# stock GAM
 OCD_mod <- ocd_gam(samples, simple = FALSE)
-# saveRDS(OCD_mod, paste0(path, 'OCD_mod.rds'))
-# OCD_mod <- readRDS(paste0(path, 'OCD_mod.rds'))
 
 var_core <- variance_component(OCD_mod, 1)
 var_site <- variance_component(OCD_mod, 2)
 
 OCD_mod_simple <- ocd_gam(samples, simple = TRUE)
-# saveRDS(OCD_mod_simple, paste0(path, 'OCD_mod_simple.rds'))
 
 # hist(resid(OCD_mod))
 # plot(resid(OCD_mod) ~ fitted(OCD_mod))
 # gam.check(OCD_mod)
 # plot(OCD_mod, pages = 1, residuals = TRUE)
 
+# Soil stock GAM
+
+# Prepare site data
+
+sites_only <- samples[!duplicated(samples$SITE_CODE), c('SITE_CODE', 'LAT', 'LON')]
+spatial_sites <- make_samples_spatial(sites_only, '+proj=longlat +datum=WGS84 +no_defs')
+
+# calculate stock
 stock_data_site <- calculate_stock(samples, OCD_mod, 'site')
 
+# collate spatial data 
 stock <- combine_other_spatial(stock_data_site, 
                                ala_file = 'chapters/Chapter2_carbon/data-raw/ALA_layers.csv',
                                catchments_file = 'chapters/Chapter2_carbon/data-raw/sites_with_catchment_data.csv')
 
 spatial_stock <- make_samples_spatial(stock, '+proj=longlat +datum=WGS84 +no_defs')
 
-sites_only <- samples[!duplicated(samples$SITE_CODE), c('SITE_CODE', 'LAT', 'LON')]
-# write.csv(sites_only, paste0(rel_path_output, 'sites_only.csv'))
-spatial_sites <- make_samples_spatial(sites_only, '+proj=longlat +datum=WGS84 +no_defs')
-# rgdal::writeOGR(spatial_sites, rel_path_output, 'spatial_sites', driver = 'ESRI Shapefile')
-
+# prepare model matrix
 model_df <- create_model_df(spatial_stock)
-# saveRDS(model_df, paste0(path, 'model_df.rds'))
 stock_covs <- covs_only(model_df)
 
+# stock GAM
 stock_mod <- stock_gam(model_df, re = TRUE)
 stock_mod_nore <- stock_gam(model_df, re = FALSE)
 
@@ -61,35 +67,32 @@ stock_mod_nore <- stock_gam(model_df, re = FALSE)
 # plot(stock_mod, pages = 1, residuals = TRUE)
 
 cv_mod <- cv(model_df)
-# saveRDS(cv_mod, paste0(path, 'cv_mod.rds'))
 cv_mod <- readRDS(paste0(path, 'cv_mod.rds'))
 
-# gam.vcomp
-# residual variance? Scale est. ?
+## Make figures
 
-# figures
-png(paste0(path, 'modified-figs/ndvi.png'), bg = 'transparent')
+png('img/ndvi.png', bg = 'transparent')
 covariate_plot('ndvi')
 dev.off()
 
-png(paste0(path, 'modified-figs/precip.png'), bg = 'transparent')
+png('img/precip.png', bg = 'transparent')
 covariate_plot('annprecip')
 dev.off()
 
-png(paste0(path, 'figs/wetland_type.png'))
+png('figs/wetland_type.png')
 bp('C_TYPE')
 dev.off()
 
-png(paste0(path, 'figs/aus_map.png'), height = 800, width = 1000, bg = 'transparent')
-aus_map(file_location = paste0(path, 'shapefiles/raw'))
+png('figs/aus_map.png', height = 800, width = 1000, bg = 'transparent')
+aus_map(file_location = 'shapefiles/raw')
 dev.off()
 
-png(paste0(path, 'figs/site_map.png'), height = 800, width = 1100)
+png('figs/site_map.png', height = 800, width = 1100)
 sites_map(spatial_sites, 
-          file_location = paste0(path, 'shapefiles/raw'))
+          file_location = 'shapefiles/raw')
 dev.off()
 
-png(paste0(path, 'modified-figs/methods_subplot.png'), 
+png(paste0(path, 'img/methods_subplot.png'), 
     height = 400, width = 500, bg = 'transparent')
 method_subplot(samples, OCD_mod)
 dev.off()
@@ -109,3 +112,9 @@ dev.off()
 png(paste0(path, 'figs/stock_mod_variables.png'), height = 1000, width = 1200)
 stock_mod_plot(model_df, stock_mod_nore)
 dev.off()
+
+# knit article
+
+knitr::knit("ms/manuscript.Rnw", output = "ms/manuscript.tex")
+tinytex::pdflatex("ms/manuscript.tex")
+
